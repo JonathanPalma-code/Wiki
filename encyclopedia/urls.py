b5dc03d16c725213ddac628095c3990278103753
Jonathan <jonathanpalma89@gmail.com>
2020-10-18 23:12:30 +0100

    chore: add Search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.subject, name="subject")
+    path("wiki/<str:title>", views.subject, name="subject"),
+    path("search/", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from . import util
 
 import markdown2
 
+body_entry_error = "The requested page was not found."
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def search(request):
+    query = request.GET.get('q')
+    entries = util.list_entries()
+    if query in entries:
+        content = markdown2.markdown(util.get_entry(query))
+        return render(request, "encyclopedia/subject.html", {
+            "title": query, 
+            "body": content,
+            "success": True
+        })
+    else:
+        matched_entry = []
+        for entry in entries:
+            if query.lower() in entry.lower():
+                matched_entry.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "query": query, 
+            "match": matched_entry,
+        })
+
 def subject(request, title):
     if util.get_entry(title):
         body_entry = markdown2.markdown(util.get_entry(title))
         return render(request, "encyclopedia/subject.html", {
             "title": title.capitalize(),
-            "entry": body_entry,
+            "body": body_entry,
             "success": True
         })
     else:
@@ -23,6 +46,6 @@ def subject(request, title):
         body_entry_error = "The requested page was not found."
         return render(request, "encyclopedia/subject.html", {
             "title": title_error, 
-            "entry": body_entry_error,
+            "body": body_entry_error,
             "success": False
         })
